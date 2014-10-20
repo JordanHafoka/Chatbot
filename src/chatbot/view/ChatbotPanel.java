b96c11dcc179f43d3d4503938c529e88a48c1b1b
JordanHafoka <mrcod4evar@yahoo.com>
2014-10-20 09:44:36 -0600

    Added a basic layout

--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,16 +1,28 @@
 package chatbot.view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import chatbot.controller.ChatbotAppController;
 
 public class ChatbotPanel extends JPanel
 {	
 	private ChatbotAppController baseController;
+	private JButton sampleButton;
+	private JTextField sampleField;
+	
+	private SpringLayout baseLayout;
 	
 	public ChatbotPanel(ChatbotAppController baseController)
 	{
 		this.baseController = baseController;
+		sampleButton = new JButton("click on me please :D");
+		sampleField = new JTextField(25);
+		baseLayout = new SpringLayout();
 		
 		setupPanel();
 		setupLayout();
@@ -18,11 +30,20 @@ public class ChatbotPanel extends JPanel
 	}
 	private void setupPanel()
 	{
-	
+		this.setBackground(Color.MAGENTA);
+		this.setLayout(baseLayout);
+		
+		this.add(sampleButton);
+		this.add(sampleField);
 	}
 	
 	private void setupLayout()
 	{
+		baseLayout.putConstraint(SpringLayout.NORTH, sampleField, 13, SpringLayout.SOUTH, sampleButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, sampleButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleButton, 126, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, sampleField, 85, SpringLayout.WEST, this);
+		
 		
 	}
 	private void setupListeners()
